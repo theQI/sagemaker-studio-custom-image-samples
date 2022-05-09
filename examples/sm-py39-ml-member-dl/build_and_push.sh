e81a50d1674062e78b538f4872a568b6dd5f1450
Laxman Panthi <3121493+theQI@users.noreply.github.com>
2022-05-08 20:22:18 -0400

    add build & push

--- examples/sm-py39-ml-member-dl/build_and_push.sh
+++ examples/sm-py39-ml-member-dl/build_and_push.sh
@@ -26,7 +26,13 @@ docker push 714686731622.dkr.ecr.us-east-2.amazonaws.com/sm-py39-ml-member-dl
 ACCOUNT_ID="714686731622"
 ECR_REPO_NAME="sm-ml-uw-custom-images"
 IMAGE_NAME="py39-member-dl"
-aws --region ${REGION} ecr get-login-password --profile medmutual | docker login --username AWS --password-stdin ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/{ECR_REPO_NAME}
-docker build . -t ${TAG_NAME} -t ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/sm-ml-uw-custom-images:${IMAGE_NAME}
-docker push ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/smstudio-custom:${TAG_NAME}
-docker push ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/sm-ml-uw-custom-images:${TAG_NAME}
+REGION="us-east-2"
+AWS_PROFILE="default"
+aws --region ${REGION} ecr get-login-password --profile ${AWS_PROFILE} | docker login --username AWS --password-stdin ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/${ECR_REPO_NAME}
+#aws --region us-east-2 ecr get-login-password --profile default | docker login --username AWS --password-stdin 714686731622.dkr.ecr.us-east-2.amazonaws.com/sm-ml-uw-custom-images
+
+docker build . -t ${IMAGE_NAME} -t ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/${ECR_REPO_NAME}:${IMAGE_NAME}
+#docker build . -t ${IMAGE_NAME} -t ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/${ECR_REPO_NAME}:${IMAGE_NAME}
+
+docker push ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/${ECR_REPO_NAME}:${IMAGE_NAME}
+#docker push 714686731622.dkr.ecr.us-east-2.amazonaws.com/sm-ml-uw-custom-images:py39-member-dl
