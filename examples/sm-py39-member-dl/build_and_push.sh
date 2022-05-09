e7e5e6f67e1813339d02dc20955a3fbf1dafc08b
Panthi <lp6360@ldcdedge01.mms.ams.local>
2022-05-08 21:18:12 -0400

    add files

--- examples/sm-py39-member-dl/build_and_push.sh
+++ examples/sm-py39-member-dl/build_and_push.sh
@@ -9,7 +9,7 @@ aws --region ${REGION} ecr get-login-password --profile ${AWS_PROFILE} | docker 
 #aws --region us-east-2 ecr get-login-password --profile default | docker login --username AWS --password-stdin 714686731622.dkr.ecr.us-east-2.amazonaws.com/sm-ml-uw-custom-images
 
 docker build . -t ${IMAGE_NAME} -t ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/${ECR_REPO_NAME}:${IMAGE_NAME}
-#docker build . -t ${IMAGE_NAME} -t ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/${ECR_REPO_NAME}:${IMAGE_NAME}
+#docker build . -t py39-member-dl -t 714686731622.dkr.ecr.us-east-2.amazonaws.com/sm-ml-uw-custom-images:py39-member-dl
 
 docker push ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/${ECR_REPO_NAME}:${IMAGE_NAME}
 #docker push 714686731622.dkr.ecr.us-east-2.amazonaws.com/sm-ml-uw-custom-images:py39-member-dl
