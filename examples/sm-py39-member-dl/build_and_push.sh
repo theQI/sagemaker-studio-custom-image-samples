618674137c2bd1ee8794b4e686ea61e23835eeb3
Laxman Panthi <Laxman.Panthi@medmutual.com>
2022-05-09 15:02:37 +0000

    add new repo

--- examples/sm-py39-member-dl/build_and_push.sh
+++ examples/sm-py39-member-dl/build_and_push.sh
@@ -1,15 +1,15 @@
 #!bash
 ACCOUNT_ID="714686731622"
-ECR_REPO_NAME="sm-ml-uw-custom-images"
+ECR_REPO_NAME="ml-sagemaker-custom-images"
 IMAGE_NAME="py39-member-dl"
 REGION="us-east-2"
 AWS_PROFILE="default"
 
 aws --region ${REGION} ecr get-login-password --profile ${AWS_PROFILE} | docker login --username AWS --password-stdin ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/${ECR_REPO_NAME}
-#aws --region us-east-2 ecr get-login-password --profile default | docker login --username AWS --password-stdin 714686731622.dkr.ecr.us-east-2.amazonaws.com/sm-ml-uw-custom-images
+#aws --region us-east-2 ecr get-login-password --profile default | docker login --username AWS --password-stdin 714686731622.dkr.ecr.us-east-2.amazonaws.com/ml-sagemaker-custom-images
 
 docker build . -t ${IMAGE_NAME} -t ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/${ECR_REPO_NAME}:${IMAGE_NAME}
-#docker build . -t py39-member-dl -t 714686731622.dkr.ecr.us-east-2.amazonaws.com/sm-ml-uw-custom-images:py39-member-dl
+#docker build . -t py39-member-dl -t 714686731622.dkr.ecr.us-east-2.amazonaws.com/ml-sagemaker-custom-images:py39-member-dl
 
 docker push ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/${ECR_REPO_NAME}:${IMAGE_NAME}
-#docker push 714686731622.dkr.ecr.us-east-2.amazonaws.com/sm-ml-uw-custom-images:py39-member-dl
+#docker push 714686731622.dkr.ecr.us-east-2.amazonaws.com/ml-sagemaker-custom-images:py39-member-dl
